6bc72e6f951a33badef68f61bf9440f6d76aa7d3
Joana Kelly <joana0713kelly@outlook.com>
2026-02-22 03:22:36 +0400

    Clean up comments in ExampleTest.php
    Removed unnecessary comments from ExampleTest class.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,12 +2,6 @@
 
 class ExampleTest extends TestCase
 {
-
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
     public function testBasicExample()
     {
         $response = $this->route('POST', 'backend.login');
